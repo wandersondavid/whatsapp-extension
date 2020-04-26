25d903d9e0318ae890224a0ca29f79ff07ec9205
Wanderson David <wandersondavid.ifg@gmail.com>
2020-04-26 15:22:28 -0300

    feat: extendios for speed changer  in audio whatsapp

---
+++ SpeedChanger/content.js
@@ -0,0 +1,19 @@
+const interval = setInterval(() => {
+    const header = document.querySelector("._3auIg")
+    if (header) {
+        console.log(header);
+        clearInterval(interval)
+
+        const button = document.createElement("button")
+        button.innerHTML = "2x"
+        button.classList.add('twoTimesButton')
+        button.addEventListener("click", () => {
+            const audios = document.querySelectorAll("audio");
+            console.log(audios)
+            audios.forEach((audio) => {
+                audio.playbackRate = 2;
+            })
+        })
+        header.appendChild(button)
+    }
+}, 1000)
